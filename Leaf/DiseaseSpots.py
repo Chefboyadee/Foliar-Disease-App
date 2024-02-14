3f56638cd953e4002a3e6a13be2af30c7f49dea0
JonathanCharles123 <157547205+JonathanCharles123@users.noreply.github.com>
2024-02-14 15:19:33 -0400

    Add files via upload

--- Leaf/DiseaseSpots.py
+++ Leaf/DiseaseSpots.py
@@ -8,7 +8,7 @@ def brown_edge_detection(image_path):
     # Convert the image to the HSV color space
     hsv_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2HSV)
 
-    # Define the lower and upper bounds for the brown color in HSV
+    # Define the lower and upper bounds for the brown,green,yellow color in HSV
     lower_brown = np.array([10, 60, 20])
     upper_brown = np.array([20, 255, 200])
 
@@ -18,14 +18,14 @@ def brown_edge_detection(image_path):
     lower_yellow = np.array([25, 60, 20])
     upper_yellow = np.array([39, 255, 200])
 
-    # Create a mask for the brown color
+    # Create a masks for the brown,green,yellow color
     brown_mask = cv2.inRange(hsv_image, lower_brown, upper_brown)
 
     green_mask = cv2.inRange(hsv_image,lower_green,upper_green)
 
     yellow_mask = cv2.inRange(hsv_image,lower_yellow,upper_yellow)
 
-    # Calculate the percentage of brown and not brown pixels
+    # Calculate the percentage of healthy and unhealthy pixels
     total_pixels = original_image.shape[0] * original_image.shape[1]
     unhealthy_percent = (np.count_nonzero(brown_mask+yellow_mask) / total_pixels) * 100
     healthy_percent = 100 - unhealthy_percent
@@ -40,17 +40,17 @@ def brown_edge_detection(image_path):
     brown_color_image = original_image.copy()
     brown_color_image[brown_mask != 0] = [0, 0, 255]  # Set brown pixels to green for visualization
 
-   # Create an image with the original color where brown pixels are detected
+   # Create an image with the original color where green pixels are detected
     green_color_image = original_image.copy()
-    green_color_image[green_mask != 0] = [0, 255, 0]  # Set brown pixels to green for visualization
+    green_color_image[green_mask != 0] = [0, 255, 0]  # Set green pixels to green for visualization
 
-    # Create an image with the original color where brown pixels are detected
+    # Create an image with the original color where yellow pixels are detected
     yellow_color_image = original_image.copy()
-    yellow_color_image[yellow_mask != 0] = [0, 255, 255]  # Set brown pixels to green for visualization
+    yellow_color_image[yellow_mask != 0] = [0, 255, 255]  # Set yellow pixels to yellow for visualization
 
     
 
-    # Display the original image, the detected edges, the brown mask, and the result
+   
     cv2.imshow('Original Image', original_image)
     cv2.imshow('Brown Color Image', brown_color_image)
     cv2.imshow('green image', green_color_image)
