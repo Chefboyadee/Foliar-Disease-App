2a50b7149c34e29920dfad84c3fc00ed44b8478f
JonathanCharles123 <157547205+JonathanCharles123@users.noreply.github.com>
2024-03-01 00:08:38 -0400

    Add files via upload

--- Leaf/DiseaseSpots.py
+++ Leaf/DiseaseSpots.py
@@ -32,7 +32,7 @@ def brown_edge_detection(image_path):
         green_mask = cv2.morphologyEx(green_mask, cv2.MORPH_CLOSE, kernel)
         yellow_mask = cv2.morphologyEx(yellow_mask, cv2.MORPH_CLOSE, kernel)
 
-        # Calculate the total number of pixels
+        #Calculate the total number of pixels
         total_pixels = original_image.shape[0] * original_image.shape[1]
 
         # Calculate the total number of pixels
@@ -50,8 +50,32 @@ def brown_edge_detection(image_path):
         unhealthy_percentage = (unhealthy_percentage / total_percentage) * 100
         healthy_percentage = (healthy_percentage / total_percentage) * 100
 
+        # Create an empty mask for the overlay
+        overlay_mask = np.zeros_like(original_image)
 
+        # Set the brown, green, and yellow regions in the overlay mask
+        overlay_mask[brown_mask != 0] = [0, 0, 255]  # Set brown pixels to red
+        overlay_mask[green_mask != 0] = [0, 255, 0]  # Set green pixels to green
+        overlay_mask[yellow_mask != 0] = [0, 255, 255]  # Set yellow pixels to yellow
 
+        # Combine brown and yellow masks
+        combined_mask = cv2.bitwise_or(brown_mask, yellow_mask)
+
+        # Create color image with the original color where brown and yellow pixels are detected
+        combined_color_image = original_image.copy()
+        combined_color_image[combined_mask != 0] = [0, 255, 255]  # Set combined pixels to yellow for visualization
+        
+        
+
+        # Apply binary thresholding on the original image to separate the leaf from the shadow
+        _, thresholded_image = cv2.threshold(combined_mask, 40, 255, cv2.THRESH_BINARY_INV)
+
+        # Convert the binary mask to a 3-channel image
+        thresholded_image_color = cv2.merge([thresholded_image, thresholded_image, thresholded_image])
+        
+
+        # Exclude green pixels from the original image using the binary mask
+        final_image = cv2.bitwise_and(original_image, cv2.bitwise_not(thresholded_image_color))
 
 
         root = tk.Tk()
@@ -64,15 +88,9 @@ def brown_edge_detection(image_path):
         healthy_label.pack()
         
 
-        # Create an empty mask for the overlay
-        overlay_mask = np.zeros_like(original_image)
+        
 
-        # Set the brown, green, and yellow regions in the overlay mask
-        overlay_mask[brown_mask != 0] = [0, 0, 255]  # Set brown pixels to red
-        overlay_mask[green_mask != 0] = [0, 255, 0]  # Set green pixels to green
-        overlay_mask[yellow_mask != 0] = [0, 255, 255]  # Set yellow pixels to yellow
-
-
+        #gui window shit
         processed_image = cv2.addWeighted(original_image, 0.5, overlay_mask, 0.5, 0)
         processed_image_rgb = cv2.cvtColor(processed_image, cv2.COLOR_BGR2RGB)
         processed_image_pil = Image.fromarray(processed_image_rgb)
@@ -82,6 +100,15 @@ def brown_edge_detection(image_path):
         processed_label.image = processed_image_tk
         processed_label.pack()
 
+        
+        Extracted_image_rgb = cv2.cvtColor(final_image, cv2.COLOR_BGR2RGB)
+        Extracted_image_pil = Image.fromarray(Extracted_image_rgb)
+        Extracted_image_tk = ImageTk.PhotoImage(Extracted_image_pil)
+
+        Extracted_label = tk.Label(root, image=Extracted_image_tk)
+        Extracted_label.image = Extracted_image_tk
+        Extracted_label.pack()
+        
 
         root.mainloop()
 
@@ -90,5 +117,5 @@ def brown_edge_detection(image_path):
 
       
 # Example usage
-input_image = ("Images/No_BG/septoria_leaf/0a68a294-30d1-4422-ab7e-a1909ec277f7___JR_Sept.L.S 8443_no_bg.png") 
+input_image = ("Images/No_BG/septoria_leaf/0af9f990-ad92-4411-8623-4498ca4805ce___Keller.St_CG 1942_no_bg.png") 
 brown_edge_detection(input_image)
